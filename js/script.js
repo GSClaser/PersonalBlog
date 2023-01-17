0cd2f97423f14a7f216bf95afb1d64ea69a760c3
Pavlov Vladimir <pav91pav@mail.ru>
2023-01-17 11:54:00 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const items = getElementByClassName(".list-nav>li>a");
-items.addEventListener("click", visible);
+const items = document.getElementsByClassName("header-blog_about");
+if (items.length > 0)
+  items[0].addEventListener("click", visible);
 
 function visible() {
-  toggle(".mobile_menu");
+  const item = document.getElementsByClassName("about-mobile")
+  if (item.length > 0)
+    item[0].classList.toggle("about-mobile_hide");
 }
