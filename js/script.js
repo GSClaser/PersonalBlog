24e88dd982816769674abfeb72031dfa136f7ce1
Pavlov Vladimir <pav91pav@mail.ru>
2023-03-03 09:54:04 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const items = document.getElementsByClassName("header-blog_about");
 if (items.length > 0)
+{
   items[0].addEventListener("click", visible);
+}
 
 function visible() {
   const item = document.getElementsByClassName("about-mobile")
   if (item.length > 0)
+  {
     item[0].classList.toggle("about-mobile_hide");
+    const item_move = document.getElementsByClassName("header-popup");
+    item_move[0].classList.toggle("header-popup_hide");
+  } 
 }
 
 const items_phone = document.getElementsByClassName("mobile-press-button");
